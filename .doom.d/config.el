1e4193d92e89cdb84138e9b90b1da64130afb7c5
Simon Christensen <c3lphie@protonmail.com>
2020-07-29 20:39:54 +0200

    Added Doom Emacs directory

---
+++ .doom.d/config.el
@@ -0,0 +1,14 @@
+(setq user-full-name "Simon Christensen"
+      user-mail-address "c3lphie@protonmail")
+
+(setq doom-font (font-spec :family "monospace" :size 15))
+
+(setq doom-theme 'doom-dracula)
+
+(setq display-line-numbers-type t)
+
+(set-frame-parameter (selected-frame) 'alpha '(100 .85))
+
+(setq org-startup-with-inline-images t)
+
+(setq org-directory "~/repositories/notes/")
