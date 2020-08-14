afabd0c2cb9d50c40bfdc5df5b68b8d3f99bfb1f
Simon Christensen <c3lphie@protonmail.com>
2020-08-14 10:59:12 +0200

    Added org capture templates

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -10,5 +10,13 @@
 (set-frame-parameter (selected-frame) 'alpha '(100 .85))
 
 (setq org-startup-with-inline-images t)
+(setq org-export-coding-system 'utf-8)
 
 (setq org-directory "~/repositories/notes/")
+
+(define-key global-map "\C-cc" 'org-capture)
+
+(setq org-capture-templates
+    '(("h" "lektier" entry (file+headline "~/repositories/notes/homework.org" "Todo")
+       "** [ ] %^{Fag}: %^{Opgave} \n DEADLINE: %^{Deadline}T")
+      ))
